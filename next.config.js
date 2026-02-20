07c03b44bf201c51d109567e2de21338f9feaff4
Rz <personalmymail09@gmail.com>
2026-02-20 13:51:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: '/code2career_ai',
-  assetPrefix: '/code2career_ai/',
+  assetPrefix: '/code2career_ai', // Removed the trailing slash
   images: {
     unoptimized: true,
   },
